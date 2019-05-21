c7f73ccf9cea61eaa060b28f66785b598f824767
Zhangtao153 <13289265979@163.com>
2019-05-21 23:51:53 +0800

    POI

---
+++ POI/src/com/zth/poi/Demo04.java
@@ -0,0 +1,27 @@
+package com.zth.poi;
+
+import org.apache.poi.hssf.extractor.ExcelExtractor;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 遍历 Excel
+ */
+public class Demo04 {
+    public static void main(String[] args) throws IOException {
+        InputStream in = new FileInputStream("g:/工作簿.xls");
+
+        POIFSFileSystem fileSystem = new POIFSFileSystem(in);
+        HSSFWorkbook workbook = new HSSFWorkbook(fileSystem);
+
+        ExcelExtractor extractor = new ExcelExtractor(workbook);
+
+        extractor.setIncludeSheetNames(false);
+        System.out.println(extractor.getText());
+    }
+}
