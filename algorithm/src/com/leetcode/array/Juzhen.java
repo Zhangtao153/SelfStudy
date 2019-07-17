0f21e713890700a54c534c14c4a805e1488966fe
Zhangtao153 <13289265979@163.com>
2019-07-18 00:24:52 +0800

    2019年7月18日00:24:34

---
+++ algorithm/src/com/leetcode/array/Juzhen.java
@@ -0,0 +1,44 @@
+package com.leetcode.array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author zth
+ * @Date 2019-07-17 21:50
+ *  螺旋矩阵
+ */
+public class Juzhen {
+    public static int[][] fill(int N){
+        int index = 1;
+        int[][] array = new int[N][N];
+        for (int loop = 0; loop < N/2; loop++) {
+            // 上
+            for (int j = loop; j < N-loop-1 ; j++) {
+                array[loop][j] = index++;
+            }
+            // 右
+            for (int i = loop; i < N-loop-1; i++) {
+                array[i][N-loop-1] = index++;
+            }
+            // 下
+            for (int j = N-loop-1; j > loop ; j--) {
+                array[N-loop-1][j] = index++;
+            }
+            // 左
+            for (int i = N-loop-1; i > loop; i--) {
+                array[i][loop] = index++;
+            }
+            if (N%2 != 0) array[N/2][N/2] = N*N;
+        }
+        return array;
+    }
+
+    public static void main(String[] args){
+        int[][] array = fill(4);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+
+    }
+}
